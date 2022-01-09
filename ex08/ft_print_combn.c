4bae64bfd45047cbfb222298ee122930fc96c1bb
Youngsu Choi <youngcho@c7r8s3.42seoul.kr>
2022-01-09 10:57:09 +0900

    ex08

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 20:53:58 by youngcho          #+#    #+#             */
-/*   Updated: 2022/01/08 21:16:10 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/01/09 10:55:07 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,35 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_recursion(int digit, int num)
+void	ft_recursion(int n, int digit, int cur_nbr, int *nbrs)
 {
+	int	i;
+
 	if (digit == 0)
 	{
-		ft_putchar(',');
-		ft_putchar(' ');
+		i = 0;
+		while (i < n)
+		{
+			ft_putchar(nbrs[i] + '0');
+			i++;
+		}
+		if (nbrs[0] != 10 - n)
+		{
+			ft_putchar(',');
+			ft_putchar(' ');
+		}
 		return ;
 	}
-	while (num <= 10 - digit)
+	while (cur_nbr <= 10 - digit)
 	{
-		ft_putchar(num + '0');
-		ft_recursion(digit - 1, ++num);
+		nbrs[n - digit] = cur_nbr++;
+		ft_recursion(n, digit - 1, cur_nbr, nbrs);
 	}
 }
 
 void	ft_print_combn(int n)
 {
-	ft_recursion(n, 0);
+	int	nbrs[10];
+
+	ft_recursion(n, n, 0, nbrs);
 }
