52259da1fb39979032acb48bb2ee15d52a0af7da
youngcho <soc4585@naver.com>
2022-01-12 16:08:19 +0900

    ex00

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 12:34:37 by youngcho          #+#    #+#             */
-/*   Updated: 2022/01/12 16:05:56 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/01/12 16:08:07 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			return ((int)s1[i] - s2[i]);
+			return ((int)(s1[i] - s2[i]));
 		i++;
 	}
 	if (s1[i] != '\0')
