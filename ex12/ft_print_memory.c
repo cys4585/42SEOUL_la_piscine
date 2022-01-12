2c0146523d22ec8f3ee082543640efd4601b2445
youngcho <soc4585@naver.com>
2022-01-12 11:58:05 +0900

    ex12 포기

---
+++ ex12/ft_print_memory.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_memory.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 11:12:32 by youngcho          #+#    #+#             */
+/*   Updated: 2022/01/12 11:57:55 by youngcho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	*ft_print_memory(void *addr, unsigned int size)
+{
+	printf("%s\n", addr);
+	printf("%d\n", size);
+	return (addr);
+}
