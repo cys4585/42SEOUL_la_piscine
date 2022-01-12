fea48825a36dc99b2efde6907cdefc1eecc9e4d3
youngcho <soc4585@naver.com>
2022-01-12 18:09:02 +0900

    ex04

---
+++ ex04/ft_strstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 17:35:10 by youngcho          #+#    #+#             */
+/*   Updated: 2022/01/12 18:08:54 by youngcho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	exist_str(char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] != '\0' && str2[i] != '\0')
+	{
+		if (str1[i] != str2[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == to_find[0] && exist_str(str + i + 1, to_find + 1))
+			return (str + i);
+		i++;
+	}
+	return (0);
+}
