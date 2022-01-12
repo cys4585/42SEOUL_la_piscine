9b7941a5f0b07de39b06d7d8f224d4a303cad030
youngcho <soc4585@naver.com>
2022-01-12 21:02:46 +0900

    ex12.....포기

--- ex12/ft_print_memory.c
+++ ex12/ft_print_memory.c
@@ -6,21 +6,11 @@
 /*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 11:12:32 by youngcho          #+#    #+#             */
-/*   Updated: 2022/01/12 11:57:55 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/01/12 21:02:35 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 void	*ft_print_memory(void *addr, unsigned int size)
 {
-	printf("%s\n", addr);
-	printf("%d\n", size);
 	return (addr);
 }
