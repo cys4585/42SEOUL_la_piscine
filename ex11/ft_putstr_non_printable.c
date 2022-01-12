c8a2abbda6ee8a55c22a1c126a152dfb2f48ddfe
youngcho <soc4585@naver.com>
2022-01-12 21:23:36 +0900

    ex11

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 19:08:19 by youngcho          #+#    #+#             */
-/*   Updated: 2022/01/12 11:11:59 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/01/12 21:23:30 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,6 @@
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
-}
-
-void	make_hex_arr(char *arr)
-{
-	int	i;
-
-	i = 0;
-	while (i < 16)
-	{
-		if (i < 10)
-			arr[i] = i + 48;
-		else
-			arr[i] = i + 87;
-		i++;
-	}
 }
 
 void	print_hex(int ascii_decimal, char *hex_arr)
@@ -42,16 +27,16 @@ void	print_hex(int ascii_decimal, char *hex_arr)
 void	ft_putstr_non_printable(char *str)
 {
 	int		i;
-	char	hex_arr[16];
+	char	*hex_arr;
 
 	i = 0;
-	make_hex_arr(hex_arr);
+	hex_arr = "0123456789abcdef";
 	while (str[i] != '\0')
 	{
-		if (0 <= str[i] && str[i] <= 31)
-			print_hex(str[i], hex_arr);
-		else
+		if (32 <= str[i] && str[i] <= 127)
 			ft_putchar(str[i]);
+		else
+			print_hex((unsigned char)str[i], hex_arr);
 		i++;
 	}
 }
