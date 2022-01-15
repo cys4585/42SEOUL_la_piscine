a105840c0b0ca6430309b95c09c74321466423b9
youngcho <soc4585@naver.com>
2022-01-15 17:55:26 +0900

    ex04 수정

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 17:35:10 by youngcho          #+#    #+#             */
-/*   Updated: 2022/01/12 18:08:54 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/01/15 17:53:28 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ char	*ft_strstr(char *str, char *to_find)
 	int	i;
 
 	i = 0;
+	if (to_find[0] == '\0')
+		return (str);
 	while (str[i] != '\0')
 	{
 		if (str[i] == to_find[0] && exist_str(str + i + 1, to_find + 1))
