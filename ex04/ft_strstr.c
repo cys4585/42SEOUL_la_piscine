42dcccb403642fa63824982aab72a2a0a7e3c7b2
youngcho <soc4585@naver.com>
2022-01-15 21:33:50 +0900

    ex04

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 17:35:10 by youngcho          #+#    #+#             */
-/*   Updated: 2022/01/15 17:53:28 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/01/15 21:33:45 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	exist_str(char *str1, char *str2)
 	int	i;
 
 	i = 0;
-	while (str1[i] != '\0' && str2[i] != '\0')
+	while (str2[i] != '\0')
 	{
 		if (str1[i] != str2[i])
 			return (0);
