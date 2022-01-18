6816f8ded31037e06c5b248643cb3757645a9c8e
youngcho <soc4585@naver.com>
2022-01-18 16:33:25 +0900

    ex02

---
+++ ex02/ft_rev_params.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 16:28:09 by youngcho          #+#    #+#             */
+/*   Updated: 2022/01/18 16:32:49 by youngcho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	while (--argc > 0)
+		ft_putstr(argv[argc]);
+}
