3959440fd1061b2d4a6974b6876cb51d5bf88c50
youngcho <soc4585@naver.com>
2022-01-18 15:59:57 +0900

    ex08

---
+++ ex08/ft_ten_queens_puzzle.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 13:00:51 by youngcho          #+#    #+#             */
+/*   Updated: 2022/01/18 15:59:45 by youngcho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print(int *map)
+{
+	int	i;
+
+	i = 0;
+	while (i < 10)
+	{
+		ft_putchar(map[i] + '0');
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int	is_valid(int *map, int idx)
+{
+	int	i;
+
+	i = 0;
+	while (i < idx)
+	{
+		if (map[i] == map[idx])
+			return (0);
+		if (map[i] + (idx - i) == map[idx])
+			return (0);
+		if (map[i] - (idx - i) == map[idx])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	rec_fn(int *map, int idx, int *cnt_ptr)
+{
+	int	val;
+
+	if (idx == 10)
+	{
+		*cnt_ptr += 1;
+		print(map);
+		return ;
+	}
+	val = 0;
+	while (val < 10)
+	{
+		map[idx] = val;
+		if (is_valid(map, idx))
+			rec_fn(map, idx + 1, cnt_ptr);
+		val++;
+	}
+}
+
+int	ft_ten_queens_puzzle(void)
+{
+	int	map[10];
+	int	cnt;
+
+	cnt = 0;
+	rec_fn(map, 0, &cnt);
+	return (cnt);
+}
