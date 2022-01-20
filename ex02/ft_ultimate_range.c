f500330e07dad994669c27ea2f30dd4e4598dded
youngcho <soc4585@naver.com>
2022-01-20 10:45:17 +0900

    ex02

---
+++ ex02/ft_ultimate_range.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 10:30:33 by youngcho          #+#    #+#             */
+/*   Updated: 2022/01/20 10:45:11 by youngcho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	*range = (int *)malloc(sizeof(int) * (max - min));
+	if (*range == NULL)
+		return (-1);
+	i = 0;
+	while (i < max - min)
+	{
+		(*range)[i] = min + i;
+		i++;
+	}
+	return (max - min);
+}	
