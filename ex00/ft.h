dd5d739d15c256df9d35b82e6a18dbcc987657de
youngcho <soc4585@naver.com>
2022-01-20 20:10:29 +0900

    ex00

--- ex00/ft.h
+++ ex00/ft.h
@@ -5,18 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/20 19:54:11 by youngcho          #+#    #+#             */
-/*   Updated: 2022/01/20 19:58:47 by youngcho         ###   ########.fr       */
+/*   Created: 2022/01/20 20:07:48 by youngcho          #+#    #+#             */
+/*   Updated: 2022/01/20 20:08:43 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	FT_H
+#ifndef FT_H
 # define FT_H
-
 void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
-int	ft_strlen(char *str);
-int	ft_strcmp(char *s1, char *s2);
-
-#endif
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
