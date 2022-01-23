5fe82171a9e343bf234517a358533a5d5bad1ec5
youngcho <soc4585@naver.com>
2022-01-23 19:44:19 +0900

    출력까지

--- ex00/main.c
+++ ex00/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
+/*   By: youngcho <youngcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 15:59:59 by youngcho          #+#    #+#             */
-/*   Updated: 2022/01/22 21:24:19 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/01/23 19:40:37 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,34 +25,52 @@ void	print(char *str)
 }
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int	main(int argc, char **argv)
 {
-	int		i;
-	char	*ref_dict_path;
 	int		is_valid;
 	int		*arr;
-	int		size;
+	int		arr_size;
+	t_dict	*dict;
+	char	**str_arr;
 
 	if (argc < 2 || argc > 3)
 	{
-		print("Error");
+		print("error");
 		return (0);
 	}
-	i = 1;
 	if (argc == 3)
 	{
-		ref_dict_path = argv[1];
-		i = 2;
+		arr = str_to_int_arr(argv[2], &is_valid, &arr_size);
+		load_reference_dict(argv[1], dict);
 	}
-	is_valid = 1;
-	arr = str_to_int_arr(argv[i], &is_valid, &size);
+	else
+		arr = str_to_int_arr(argv[1], &is_valid, &arr_size);
 	if (!is_valid)
 	{
-		print("Error");
+		print("error");
 		return (0);
 	}
-	for (int i = 0; i < size; i++)
-		printf("arr[%d]: %d\n", i, arr[i]);
+	dict = load_init_dict();
+	if (dict == NULL)
+	{
+		print("Dict Error");
+		return (0);
+	}
+	str_arr = resolve(dict, &arr, arr_size);
+	
+	free(arr);
+	t_dict	*tmp;
+	while (dict)
+	{
+		tmp = dict;
+		free(tmp->value);
+		free(tmp);
+		dict = dict->next;
+	}
+	free(str_arr);
+	//system("leaks a.out");
 	return (0);
 }
