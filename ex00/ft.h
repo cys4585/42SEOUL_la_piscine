c488de8915e6aa1fa9472285402f7df5d17efe78
youngcho <soc4585@naver.com>
2022-01-25 16:45:02 +0900

    ex00

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,7 +6,7 @@
 /*   By: youngcho <youngcho@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 20:07:48 by youngcho          #+#    #+#             */
-/*   Updated: 2022/01/20 20:08:43 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/01/25 16:39:59 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,4 @@ void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
+#endif
